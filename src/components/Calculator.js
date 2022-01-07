c9fac35a1755e8b65040a507bf8e857198140322
rtonata88 <rtonata@gmail.com>
2022-01-07 20:19:16 +0200

    Implement the calculate operate function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,27 +1,73 @@
-import React from 'react';
-import Screen from './Screen';
-import Buttons from './Buttons';
+import React from 'react'
+import calculate from '../logic/calculate'
+import Screen from './Screen'
 
 class Calculator extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
+      calculateObj: {
+        total: '0',
+        next: null,
+        operation: null,
+      },
+    }
+  }
 
-    };
+  handleClick = (e) => {
+    const { calculateObj } = this.state
+    this.setState({
+      calculateObj: calculate(calculateObj, e.target.dataset.clicked),
+    })
   }
 
   render() {
+    const mainButtons = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.']
+    const buttonsDisplay = []
+    mainButtons.forEach((button) => {
+      if (button === '0') {
+        buttonsDisplay.push(<button type="button" className="col-md-8 Buttons p-4" key={button} data-clicked={button} onClick={this.handleClick}>{button}</button>)
+      } else {
+        buttonsDisplay.push(<button type="button" className="col-md-4 Buttons p-4" key={button} data-clicked={button} onClick={this.handleClick}>{button}</button>)
+      }
+    })
+
+    const arithmeticButtons = ['÷', 'x', '-', '+', '=']
+    const arithmeticButtonsDisplay = []
+    arithmeticButtons.forEach((button) => {
+      arithmeticButtonsDisplay.push(<button type="button" className="col-md-12 Arithmetic-buttons p-4" key={button} data-clicked={button} onClick={this.handleClick}>{button}</button>)
+    })
+
+    const { calculateObj } = this.state
+
     return (
       <div className="row">
         <div className="col-lg-4 col-md-4 mx-auto border">
           <div className="Calculator-container">
-            <Screen />
-            <Buttons />
+            <div className="row">
+              <Screen
+                total={calculateObj.total}
+                next={calculateObj.next}
+                operation={calculateObj.operation}
+              />
+            </div>
+            <div className="row Buttons-container">
+              <div className="col-md-10">
+                <div className="row">
+                  {buttonsDisplay}
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="row">
+                  {arithmeticButtonsDisplay}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Calculator;
+export default Calculator
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,7 +1,7 @@
-import operate from './operate';
+import operate from './operate'
 
 function isNumber(item) {
-  return !!item.match(/[0-9]+/);
+  return !!item.match(/[0-9]+/)
 }
 
 /**
@@ -19,50 +19,50 @@ export default function calculate(obj, buttonName) {
       total: null,
       next: null,
       operation: null,
-    };
+    }
   }
 
   if (isNumber(buttonName)) {
     if (buttonName === '0' && obj.next === '0') {
-      return {};
+      return {}
     }
     // If there is an operation, update next
     if (obj.operation) {
       if (obj.next) {
-        return { ...obj, next: obj.next + buttonName };
+        return { ...obj, next: obj.next + buttonName }
       }
-      return { ...obj, next: buttonName };
+      return { ...obj, next: buttonName }
     }
     // If there is no operation, update next and clear the value
     if (obj.next) {
       return {
         next: obj.next + buttonName,
         total: null,
-      };
+      }
     }
     return {
       next: buttonName,
       total: null,
-    };
+    }
   }
 
   if (buttonName === '.') {
     if (obj.next) {
       if (obj.next.includes('.')) {
-        return { ...obj };
+        return { ...obj }
       }
-      return { ...obj, next: `${obj.next}.` };
+      return { ...obj, next: `${obj.next}.` }
     }
     if (obj.operation) {
-      return { next: '0.' };
+      return { next: '0.' }
     }
     if (obj.total) {
       if (obj.total.includes('.')) {
-        return {};
+        return {}
       }
-      return { total: `${obj.total}.` };
+      return { total: `${obj.total}.` }
     }
-    return { total: '0.' };
+    return { total: '0.' }
   }
 
   if (buttonName === '=') {
@@ -71,20 +71,20 @@ export default function calculate(obj, buttonName) {
         total: operate(obj.total, obj.next, obj.operation),
         next: null,
         operation: null,
-      };
+      }
     }
     // '=' with no operation, nothing to do
-    return {};
+    return {}
   }
 
   if (buttonName === '+/-') {
     if (obj.next) {
-      return { ...obj, next: (-1 * parseFloat(obj.next)).toString() };
+      return { ...obj, next: (-1 * parseFloat(obj.next)).toString() }
     }
     if (obj.total) {
-      return { ...obj, total: (-1 * parseFloat(obj.total)).toString() };
+      return { ...obj, total: (-1 * parseFloat(obj.total)).toString() }
     }
-    return {};
+    return {}
   }
 
   // Button must be an operation
@@ -97,27 +97,27 @@ export default function calculate(obj, buttonName) {
 
   // User pressed an operation after pressing '='
   if (!obj.next && obj.total && !obj.operation) {
-    return { ...obj, operation: buttonName };
+    return { ...obj, operation: buttonName }
   }
 
   // User pressed an operation button and there is an existing operation
   if (obj.operation) {
     if (obj.total && !obj.next) {
-      return { ...obj, operation: buttonName };
+      return { ...obj, operation: buttonName }
     }
 
     return {
       total: operate(obj.total, obj.next, obj.operation),
       next: null,
       operation: buttonName,
-    };
+    }
   }
 
   // no operation yet, but the user typed one
 
   // The user hasn't typed a number yet, just save the operation
   if (!obj.next) {
-    return { operation: buttonName };
+    return { operation: buttonName }
   }
 
   // save the operation and shift 'next' into 'total'
@@ -125,5 +125,5 @@ export default function calculate(obj, buttonName) {
     total: obj.next,
     next: null,
     operation: buttonName,
-  };
+  }
 }
