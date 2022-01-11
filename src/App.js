b65b209b746b02aaae687da4e729a72b92e503ec
rtonata88 <rtonata@gmail.com>
2022-01-11 22:05:59 +0200

    Add Navbar.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="py-5 text-center container">
+    <div className="py-3 container">
       <div className="row">
         <Calculator />
       </div>
