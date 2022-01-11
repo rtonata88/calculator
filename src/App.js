92a2976854d735a282980cfb5b150509c30f3425
rtonata88 <rtonata@gmail.com>
2022-01-11 22:32:57 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 import Calculator from './components/Calculator'
 import Home from './components/Home'
 import Quote from './components/Quote'
@@ -8,11 +10,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="py-3 container">
-      <Home />
-      <div className="row">
-        <Calculator />
-      </div>
-      <Quote />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quote />} />
+      </Routes>
     </div>
   )
 }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const Quote = () => (
-  <div className='col-md-5 p-lg-5 mx-auto my-5'>
-    <p className='lead fw-normal'>
+  <div className="col-md-5 p-lg-5 mx-auto my-5">
+    <p className="lead fw-normal">
       Mathematics consists of proving the most obvious thing in the least
       obvious way.
     </p>
   </div>
-);
+)
 
-export default Quote;
+export default Quote
