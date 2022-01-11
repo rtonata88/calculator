2f33e172f5ebcdd5ae6e9447df76d9579b0a93d1
rtonata88 <rtonata@gmail.com>
2022-01-11 22:12:43 +0200

    Add Home.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Calculator from './components/Calculator'
+import Home from './components/Home'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="py-3 container">
+      <Home />
       <div className="row">
         <Calculator />
       </div>
