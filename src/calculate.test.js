a4d3f371a25eb017f14b9c27b6c7f45c42b7cc47
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-13 15:13:10 -0500

    Added testing for calculate.js

---
+++ src/calculate.test.js
@@ -0,0 +1,64 @@
+import calculate from './logic/calculate';
+
+describe('Test for Calculate library',() => {
+  test.each([{
+    mockObj : {
+      total: '20',
+      next: '10',
+      operation: '+',
+    },
+    operation : '=',
+    result : {
+      total:'30',
+      next: null,
+      operation: null,
+    },
+  },
+
+  {
+    mockObj : {
+      total: '20',
+      next: '10',
+      operation: '-',
+    },
+    operation : '=',
+    result : {
+      total:'10',
+      next: null,
+      operation: null,
+    },
+  },
+
+  {
+    mockObj : {
+      total: '20',
+      next: '10',
+      operation: 'x',
+    },
+    operation : '=',
+    result : {
+      total:'200',
+      next: null,
+      operation: null,
+    },
+  },
+
+  {
+    mockObj : {
+      total: '20',
+      next: '10',
+      operation: '+',
+    },
+    operation : 'AC',
+    result : {
+      total:null,
+      next: null,
+      operation: null,
+    },
+  },
+
+  ])('Add both numbers',(({mockObj,operation,result}) => {
+    
+    return expect(calculate(mockObj,operation)).toEqual(result)
+  }))
+});
